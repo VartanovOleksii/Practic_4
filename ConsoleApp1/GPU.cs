ee4f66d372befab1c819893d152493c0e7481fe5
PC <PC@DESKTOP-7DJ275F>
2025-11-21 19:53:14 +0100

    Add project files.

---
+++ ConsoleApp1/GPU.cs
@@ -0,0 +1,226 @@
+﻿using System.Text.RegularExpressions;
+using System.Xml;
+
+public class Gpu
+{
+    //Приватні змінні
+    private string _modelName;
+    private int _gpuClock;
+    private GPUArchitecture _architecture;
+    private int _memorySize;
+    private DateTime _releaseDate;
+    private short _memoryBusWidth;
+    private decimal _launchPrice;
+
+    //Приватні статичні значення
+    private static int _counter;
+    private static decimal _discount;
+    
+    //Публічні дефолтні значення
+    public const string DefName = "DefaultName";
+    public const int DefClock = 1000;
+    public const GPUArchitecture DefArchitecture = GPUArchitecture.Turing;
+    public const int DefMemory = 1;
+    public const short DefBus = 128;
+    public const decimal DefPrice = 0.01m;
+
+
+    //Публічні властивості
+    public bool InBasket { get; private set; } = true;
+
+    public string ModelName
+    {
+        get => _modelName;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Назва моделі не може бути порожньою.");
+
+            if (value.Length < 5 || value.Length > 40)
+                throw new ArgumentException("Довжина назви моделі має бути від 5 до 40 символів.");
+
+            if (!Regex.IsMatch(value, @"^[a-zA-Z0-9\s]+$"))
+                throw new ArgumentException("Назва моделі може містити лише латинські літери та цифри.");
+
+            _modelName = value;
+        }
+    }
+
+    public int GpuClock
+    {
+        get => _gpuClock;
+        set
+        {
+            if (value < 1000 || value > 4000)
+                throw new ArgumentException("Частота GPU має бути в діапазоні 1000-4000 МГц.");
+            _gpuClock = value;
+        }
+    }
+
+    public GPUArchitecture Architecture
+    {
+        get => _architecture;
+        set
+        {
+            if (!Enum.IsDefined(typeof(GPUArchitecture), value))
+            {
+                throw new ArgumentException("Архітектура не коректна!");
+            }
+            _architecture = value;
+        }
+    }
+
+    public int MemorySize
+    {
+        get => _memorySize;
+        set
+        {
+            if (value < 1 || value > 32)
+                throw new ArgumentException("Об'єм пам'яті має бути в діапазоні 1–32 ГБ.");
+            _memorySize = value;
+        }
+    }
+
+    public DateTime ReleaseDate
+    {
+        get => _releaseDate;
+        set
+        {
+            if (value > DateTime.Now)
+                throw new ArgumentException("Дата випуску не може бути у майбутньому.");
+            _releaseDate = value;
+        }
+    }
+
+    public short MemoryBusWidth
+    {
+        get => _memoryBusWidth;
+        set
+        {
+            if (value < 128 || value > 2048)
+                throw new ArgumentException("Розрядність шини має бути в діапазоні 128-2048 біт.");
+            _memoryBusWidth = value;
+        }
+    }
+
+    public decimal LaunchPrice
+    {
+        get => _launchPrice;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("Ціна на релізі має бути більше 0.");
+            _launchPrice = value;
+        }
+    }
+
+
+    //Публічні статичні властивості
+    public static int Counter => _counter;
+
+    public static decimal Discount
+    {
+        get { return _discount; }
+        set
+        {
+            if (value > 0 && value < 1)
+            {
+                _discount = value;
+            }
+            else _discount = 0;
+        }
+    }
+
+
+    //Публічні методи
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Модель: {ModelName}");
+        Console.WriteLine($"GPU Clock: {GpuClock} МГц");
+        Console.WriteLine($"Архітектура: {Architecture}");
+        Console.WriteLine($"Пам'ять: {MemorySize} ГБ");
+        Console.WriteLine($"Розрядність шини: {MemoryBusWidth} біт");
+        Console.WriteLine($"Дата випуску: {ReleaseDate.ToShortDateString()}");
+        Console.WriteLine($"Ціна на релізі: {LaunchPrice} $");
+        if (InBasket) { Console.WriteLine("Відеокарта знаходиться в кошику"); }
+        else { Console.WriteLine("Відеокарта не знаходиться в кошику"); }
+    }
+
+    public int YearsSinceRelease()
+    {
+        return DateTime.Now.Year - ReleaseDate.Year;
+    }
+
+    public int YearsSinceRelease(DateTime selectedDate)
+    {
+        return selectedDate.Year - ReleaseDate.Year;
+    }
+
+    public void AddToBasket()
+    {
+        if (!InBasket)
+        {
+            InBasket = true;
+            Console.WriteLine("Відеокарта додана в кошик");
+        }
+        else
+        {
+            Console.WriteLine("Відеокарта вже знаходиться в кошику");
+        }
+    }
+
+    public void DeleteFromBasket()
+    {
+        if (InBasket)
+        {
+            InBasket = false;
+            Console.WriteLine("Відеокарта видалена з кошика");
+        }
+        else
+        {
+            Console.WriteLine("Відеокарти не було в кошику");
+        }
+    }
+
+
+    //Публічні статичні методи
+    public static decimal PriceWithDiscount(decimal price)
+    {
+        return price * (1 - Discount);
+    }
+
+
+    //Статичний конструктор
+    static Gpu()
+    {
+        Console.WriteLine("Використовується статичний конструктор");
+        Discount = 0.15m;
+    }
+
+
+    //Конструктори
+    public Gpu() : this(DefName, DefClock, DefArchitecture, DefMemory, DateTime.MinValue, DefBus, DefPrice)
+    {
+        Console.WriteLine("Використовується конструктор без параметрів.");
+    }
+
+    public Gpu(string modelName, GPUArchitecture architecture, decimal launchPrice) : this(modelName, DefClock, architecture, DefMemory, DateTime.MinValue, DefBus, launchPrice)
+    {
+        Console.WriteLine("Використовується конструктор з параметрами: назва, архітектура, ціна на релізі.");
+    }
+
+    public Gpu(string modelName, int gpuClock, GPUArchitecture architecture, int memorySize, DateTime releaseDate, short memoryBusWidth, decimal launchPrice)
+    {
+        Console.WriteLine("Використовується конструктор зі всіма параметрами.");
+
+        _counter++;
+
+        ModelName = modelName;
+        GpuClock = gpuClock;
+        Architecture = architecture;
+        MemorySize = memorySize;
+        ReleaseDate = releaseDate;
+        MemoryBusWidth = memoryBusWidth;
+        LaunchPrice = launchPrice;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.JavaScript;
+
+System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+int maxCount = 0;
+while (true)
+{
+    Console.Write("Введіть максимальну кількість відеокарт (N > 0): ");
+
+    if (int.TryParse(Console.ReadLine(), out maxCount) && maxCount > 0)
+        break;
+
+    Console.WriteLine("Помилка: введіть додатне число!");
+}
+
+List<Gpu> gpus = new List<Gpu>(maxCount);
+
+start_of_loop:
+while (true)
+{
+    Console.WriteLine("\n==== МЕНЮ ====");
+    Console.WriteLine("1 - Додати об'єкт");
+    Console.WriteLine("2 - Переглянути додані об'єкти");
+    Console.WriteLine("3 - Знайти об'єкт");
+    Console.WriteLine("4 - Демонстрація поведінки");
+    Console.WriteLine("5 - Видалити об'єкт");
+    Console.WriteLine("0 - Вихід");
+    Console.Write("Ваш вибір -> ");
+
+    string choice = Console.ReadLine();
+    Console.WriteLine();
+
+    switch (choice)
+    {
+        //Додати об'єкт
+        case "1":
+            while (true)
+            {
+                if (Gpu.Counter >= maxCount)
+                {
+                    Console.WriteLine("Досягнута максимальна кількість об'єктів!");
+                    break;
+                }
+
+                Console.WriteLine("\n==== МЕНЮ ====");
+                Console.WriteLine("1 - Задано користувачем");
+                Console.WriteLine("2 - Тест конструкторів");
+                Console.WriteLine("0 - Назад");
+                Console.Write("Ваш вибір -> ");
+
+                string selectchoice = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (selectchoice)
+                {
+                    case "1":
+                        try
+                        {
+                            Gpu vcard = AddGPU();
+                            gpus.Add(vcard);
+                            Console.WriteLine("Відеокарта успішно додана!");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Помилка: {ex.Message}");
+                        }
+                        break;
+
+                    case "2":
+                        Random seed = new Random();
+                        int constructor = seed.Next(3);
+
+                        Gpu card = null;
+
+                        switch (constructor)
+                        {
+                            case 0:
+                                try
+                                {
+                                    card = new Gpu();
+                                    gpus.Add(card);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.ToString());
+                                }
+
+                                if (card == null)
+                                {
+                                    Console.WriteLine("Об'єкт не було створено");
+                                }
+
+                                break;
+
+                            case 1:
+                                try
+                                {
+                                    card = new Gpu("GeForce RTX 5060 Ti", GPUArchitecture.Blackwell, 429.99m);
+                                    gpus.Add(card);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.ToString());
+                                }
+
+                                if (card == null)
+                                {
+                                    Console.WriteLine("Об'єкт не було створено");
+                                }
+
+                                break;
+
+                            case 2:
+                                try
+                                {
+                                    var rDate = new DateTime(2025, 04, 16);
+                                    card = new Gpu("GeForce RTX 5060 Ti", 2602, GPUArchitecture.Blackwell, 16, rDate, 128, 429.99m);
+                                    gpus.Add(card);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.ToString());
+                                }
+
+                                if (card == null)
+                                {
+                                    Console.WriteLine("Об'єкт не було створено");
+                                }
+
+                                break;
+                        }
+
+                        break;
+
+                    case "0":
+                        goto start_of_loop;
+                        break;
+
+                    default:
+                        Console.WriteLine("Неправильний вибір, спробуйте знову.");
+                        break;
+                }
+            }
+
+            break;
+
+        //Переглянути додані об'єкти
+        case "2":
+            if (gpus.Count == 0)
+            {
+                Console.WriteLine("Список порожній.");
+            }
+            else
+            {
+                Console.WriteLine("Список відеокарт:");
+                foreach (var card in gpus)
+                {
+                    card.PrintInfo();
+                    Console.WriteLine("\n");
+                }
+            }
+            break;
+
+        //Знайти об'єкт
+        case "3":
+            while (true)
+            {
+                Console.WriteLine("Виберіть параметр для пошуку:");
+                Console.WriteLine("1 - Назва");
+                Console.WriteLine("2 - Архітектура");
+                Console.WriteLine("0 - Назад");
+                Console.Write("Ваш вибір -> ");
+
+                List<Gpu> findresults = new List<Gpu>();
+
+                string findchoice = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (findchoice)
+                {
+                    case "1":
+                        Console.Write("Введіть назву моделі: ");
+                        string searchName = Console.ReadLine();
+                        findresults = gpus.FindAll(vc => vc.ModelName.Contains(searchName, StringComparison.OrdinalIgnoreCase));
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Доступні архітектури:");
+                        foreach (var arch in Enum.GetValues(typeof(GPUArchitecture)))
+                            Console.WriteLine($"- {arch}");
+
+                        Console.Write("Введіть архітектуру: ");
+                        string searchArch = Console.ReadLine();
+
+                        if (Enum.TryParse(searchArch, true, out GPUArchitecture archValue))
+                        {
+                            findresults = gpus.FindAll(vc => vc.Architecture == archValue);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Помилка: некоректна архітектура!");
+                        }
+                        break;
+
+                    case "0":
+                        goto start_of_loop;
+
+                    default:
+                        Console.WriteLine("Неправильний вибір, спробуйте знову.");
+                        break;
+                }
+
+                Console.WriteLine("\n");
+
+                if (findresults.Count > 0)
+                {
+                    foreach (Gpu res in findresults)
+                    {
+                        res.PrintInfo();
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Об'єкт не знайдено");
+                }
+            }
+
+        //Демонстрація поведінки
+        case "4":
+            if (gpus.Count == 0)
+            {
+                Console.WriteLine("Додайте об'єкти для демонстрації поведінки");
+            }
+            else
+            {
+                while (true)
+                {
+                    Console.WriteLine("\n==== МЕНЮ ====");
+                    Console.WriteLine("1 - Переглянути характеристики");
+                    Console.WriteLine("2 - Кількість років з релізу");
+                    Console.WriteLine("3 - Кількість років від релізу до заданої дати");
+                    Console.WriteLine("4 - Додати до кошику");
+                    Console.WriteLine("5 - Видалити з кошику");
+                    Console.WriteLine("0 - Назад");
+                    Console.Write("Ваш вибір -> ");
+
+                    string subchoice = Console.ReadLine();
+                    Console.WriteLine();
+
+                    switch (subchoice)
+                    {
+                        case "1":
+                            gpus[0].PrintInfo();
+                            break;
+
+                        case "2":
+                            Console.WriteLine($"Пройшло {gpus[0].YearsSinceRelease()} років");
+                            break;
+
+                        case "3":
+                            Console.Write("Введіть дату: ");
+                            var selectedDate = new DateTime();
+
+                            try
+                            {
+                                if (DateTime.TryParse(Console.ReadLine(), out selectedDate))
+                                {
+                                    Console.WriteLine($"Пройшло {gpus[0].YearsSinceRelease(selectedDate)} років");
+                                }
+                                else
+                                {
+                                    throw new ArgumentException("Дата не коректна!");
+                                }
+                            }
+                            catch (ArgumentException)
+                            {
+                                Console.WriteLine("Дата не коректна!");
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+
+
+                            break;
+
+                        case "4":
+                            gpus[0].AddToBasket();
+                            break;
+
+                        case "5":
+                            gpus[0].DeleteFromBasket();
+                            break;
+
+                        case "0":
+                            goto start_of_loop;
+                            break;
+
+                        default:
+                            Console.WriteLine("Неправильний вибір, спробуйте знову.");
+                            break;
+                    }
+                }
+            }
+            break;
+
+        //Видалити об'єкт
+        case "5":
+            if (gpus.Count == 0)
+            {
+                Console.WriteLine("Список порожній.");
+                break;
+            }
+            while (true)
+            {
+                Console.WriteLine("Виберіть параметр видалення:");
+                Console.WriteLine("1 - Номер");
+                Console.WriteLine("2 - Назва");
+                Console.WriteLine("0 - Назад");
+                Console.Write("Ваш вибір -> ");
+
+                string deletechoice = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (deletechoice)
+                {
+                    case "1":
+                        int index;
+
+                        while (true)
+                        {
+                            Console.Write("Введіть номер об'єкту для видалення: ");
+
+                            if (int.TryParse(Console.ReadLine(), out index) && index >= 0 && index < gpus.Count())
+                                break;
+
+                            Console.WriteLine("Помилка: введіть корректний індекс!");
+                        }
+
+                        gpus.RemoveAt(index);
+                        Console.WriteLine("Об'єкт видалено.");
+                        break;
+
+                    case "2":
+                        Console.Write("Введіть назву моделі для видалення:");
+                        string deleteName = Console.ReadLine();
+
+                        var removed = gpus.RemoveAll(vc => vc.ModelName.Equals(deleteName, StringComparison.OrdinalIgnoreCase));
+                        Console.WriteLine(removed > 0 ? "Об'єкт видалено." : "Об'єкт не знайдено.");
+                        break;
+
+                    case "0":
+                        goto start_of_loop;
+
+                    default:
+                        Console.WriteLine("Неправильний вибір, спробуйте знову.");
+                        break;
+                }
+            }
+
+        //Вихід
+        case "0":
+            Console.WriteLine("Вихід із програми...");
+            return;
+
+        default:
+            Console.WriteLine("Неправильний вибір, спробуйте знову.");
+            break;
+    }
+}
+
+static Gpu AddGPU()
+{
+    Gpu vc = new Gpu();
+
+
+    Console.Write("Введіть назву моделі: ");
+    vc.ModelName = Console.ReadLine();
+
+
+    Console.Write("Введіть частоту GPU (1000–4000): ");
+    vc.GpuClock = int.Parse(Console.ReadLine());
+
+
+    GPUArchitecture architecture;
+    Console.WriteLine("Виберіть архітектуру: ");
+    foreach (var arch in Enum.GetValues(typeof(GPUArchitecture)))
+        Console.WriteLine($"- {arch}");
+    Console.Write("Ваш вибір: ");
+    if (Enum.TryParse<GPUArchitecture>(Console.ReadLine(), true, out architecture))
+    {
+        vc.Architecture = architecture;
+    }
+    else
+    {
+        throw new ArgumentException("Архітектура не коректна!");
+    }
+
+
+    Console.Write("Введіть обсяг пам'яті (1–32 ГБ): ");
+    vc.MemorySize = int.Parse(Console.ReadLine());
+
+
+    DateTime releaseDate;
+    Console.Write("Введіть дату випуску: ");
+    if (DateTime.TryParse(Console.ReadLine(), out releaseDate))
+    {
+        vc.ReleaseDate = releaseDate;
+    }
+    else
+    {
+        throw new ArgumentException("Дата не коректна!");
+    }
+
+
+    Console.Write("Введіть розрядність шини (128–2048 біт): ");
+    vc.MemoryBusWidth = short.Parse(Console.ReadLine());
+
+
+    Console.Write("Введіть ціну на релізі (>0$): ");
+    vc.LaunchPrice = decimal.Parse(Console.ReadLine());
+
+
+    return vc;
+}
